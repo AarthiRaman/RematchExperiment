08ff920ed1593d6c464ac332e5546b6f3f974bc3
Aarthi Raman <aarthiraman@192-168-1-3.tpgi.com.au>
2018-05-27 17:19:29 +1000

    getting the text aligned

--- src/Components/Wheel/Wheel.js
+++ src/Components/Wheel/Wheel.js
@@ -14,6 +14,18 @@ const getSectorPath = (x, y, outerDiameter, a1, a2) => {
     return "M" + x + " " + y + " " + cx1 + " " + cy1 + " A" + cr + " " + cr + " 0 0 1 " + cx2 + " " + cy2 + "Z";
 }
 
+const getTextPath = (x, y, outerDiameter, a1, a2) => {
+    const degtorad = Math.PI / 180;
+    const halfOuterDiameter = outerDiameter / 2;
+    const cr = halfOuterDiameter - 5;
+    const cx1 = (Math.cos(degtorad * a2) * cr) + x;
+    const cy1 = (-Math.sin(degtorad * a2) * cr) + y;
+    const cx2 = (Math.cos(degtorad * a1) * cr) + x;
+    const cy2 = (-Math.sin(degtorad * a1) * cr) + y;
+
+    return "M" + x + " " + y;
+}
+
 const calculateAngle = (totalSections, sectionNo) => {
     const sectLength = 360 / totalSections;
 
@@ -27,16 +39,18 @@ export default function Wheel({ form, wheel }) {
     const outerDiameter=575;
     const validEntries = form.wheel.status ? form.inputs.filter((entry) => entry.value.length > 0 ? entry.value : null) : [];
     const entryArrays = validEntries.length > 0 ? validEntries.map((entry) => entry.value) : Array(12).fill('Your options');
-    const colorWheel = ['#c51b8a', '#fa9fb5', '#fde0dd'];
+    const colorWheel = ['#c51b8a', '#fa9fb5', '#fde0dd', '#f1ddcf', '#ffddf4', '#ffb3de'];
 
     return <div className="wheel__wrapper">
     <svg width={outerDiameter} height={outerDiameter}>
         {entryArrays.join(',')}
            {
             entryArrays.map((val, i) => <g>
-                <path id={`arc${i}`} d={getSectorPath(outerDiameter / 2, outerDiameter / 2, outerDiameter, calculateAngle(entryArrays.length, i).start, calculateAngle(entryArrays.length, i).end)} fill={colorWheel[i % 3]}/>
+                <path d={getSectorPath(outerDiameter / 2, outerDiameter / 2, outerDiameter, calculateAngle(entryArrays.length, i).start, calculateAngle(entryArrays.length, i).end)} fill={colorWheel[i % colorWheel.length]}/>
+                <path id={`arc${i}`} d={getTextPath("M 100,200 H 200,300", calculateAngle(entryArrays.length, (i+0.75)).end)} fill={"#000"} />
+                
                 <text>
-                <textPath startOffset={"60%"} side="right" href={`#arc${i}`}>
+                <textPath startOffset={"70%"} href={`#arc${i}`}>
                   {val}
                 </textPath>
               </text>
